077ea0d2106b923b330a08c89ef3808bb3697026
Vincenzofdg <vfdgiacomo@gmail.com>
2022-05-02 00:21:05 -0300

    Updates

---
+++ precache-manifest.288a35f6fd046f46992e868bfbab6ce6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777d8454184ed81e528cdeb37686a529",
+    "url": "/Menu-Recipes/index.html"
+  },
+  {
+    "revision": "ce49941083d470093b6a",
+    "url": "/Menu-Recipes/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "d7473a638674f53eb5fc",
+    "url": "/Menu-Recipes/static/css/main.d3bc1e96.chunk.css"
+  },
+  {
+    "revision": "ce49941083d470093b6a",
+    "url": "/Menu-Recipes/static/js/2.505c347b.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/Menu-Recipes/static/js/2.505c347b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7473a638674f53eb5fc",
+    "url": "/Menu-Recipes/static/js/main.36799edd.chunk.js"
+  },
+  {
+    "revision": "f49965dd06df1cf074e9",
+    "url": "/Menu-Recipes/static/js/runtime-main.9be490ec.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/Menu-Recipes/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/Menu-Recipes/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/Menu-Recipes/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "c15ea66c4cf4044ff328ceb3633d5fd8",
+    "url": "/Menu-Recipes/static/media/fundoApp.c15ea66c.jpg"
+  },
+  {
+    "revision": "c3356a6110cd1253117160e073d28924",
+    "url": "/Menu-Recipes/static/media/loginGif.c3356a61.gif"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/Menu-Recipes/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/Menu-Recipes/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/Menu-Recipes/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/Menu-Recipes/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/Menu-Recipes/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
